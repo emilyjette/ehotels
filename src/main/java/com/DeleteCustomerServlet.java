d07bad2a7aa19796ab18c8ec03986cbcc8f5f701
cyoun00 <cyoun065@uottawa.ca>
2025-04-04 19:25:18 -0400

    fixed changeCustomerPage

---
+++ src/main/java/com/DeleteCustomerServlet.java
@@ -0,0 +1,32 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Date;
+
+@WebServlet("/DeleteCustomerServlet")
+public class DeleteCustomerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String ID = request.getParameter("custID");
+        int intCustID = Integer.parseInt(ID);
+
+        CustomerService customerService = new CustomerService();
+
+        try {
+
+            String resultMessage = customerService.deleteCustomer(intCustID);
+
+            // Pass the result message to the JSP
+            request.setAttribute("message", resultMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("message", "Error creating customer: " + e.getMessage());
+        }
+    }
+}
